367fc5963bb81e4e92022a580d44d78845452124
Raushan12345comp <60733038+Raushan12345comp@users.noreply.github.com>
2021-06-06 15:32:57 +0530

    first-commit

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyB1xcrbuyFml0MB1HqbW9lGVrtDsa5tIfc",
+    authDomain: "mygitapp5.firebaseapp.com",
+    projectId: "mygitapp5",
+    storageBucket: "mygitapp5.appspot.com",
+    messagingSenderId: "531406972957",
+    appId: "1:531406972957:web:ad7872e98561e85f38bef4",
+    measurementId: "G-G61SJDQXPZ"
+  };
